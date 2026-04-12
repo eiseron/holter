4ed4b7008b3e13148e20cc40df9ff1072d7e1fcf
Guilherme Isaías Silva <guilherme@eiseron.com>
2026-04-12 23:32:41 +0000

    security: harden API status filtering and pagination parsing

--- lib/holter_web/api/controllers/monitor_log_controller.ex
+++ lib/holter_web/api/controllers/monitor_log_controller.ex
@@ -104,9 +104,17 @@ defmodule HolterWeb.Api.MonitorLogController do
 
   defp maybe_put_integer(acc, params, key, atom_key) do
     case Map.get(params, key) do
-      val when is_binary(val) -> Map.put(acc, atom_key, String.to_integer(val))
-      val when is_integer(val) -> Map.put(acc, atom_key, val)
-      _ -> acc
+      val when is_binary(val) ->
+        case Integer.parse(val) do
+          {int, ""} -> Map.put(acc, atom_key, int)
+          _ -> acc
+        end
+
+      val when is_integer(val) ->
+        Map.put(acc, atom_key, val)
+
+      _ ->
+        acc
     end
   end
 
